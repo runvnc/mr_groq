a83fd541bddd2da8ee4cbf8475319713a10bba6c
Jason Livesay <runvnc@gmail.com>
2025-11-14 04:01:37 -0600

    dont drop first user message

--- src/mr_groq/mod.py
+++ src/mr_groq/mod.py
@@ -40,13 +40,13 @@ async def stream_chat(model, messages=[], context=None, num_ctx=200000,
         # if the role of both of them is the same
         # this is not valid
         # so we need to remove the last message
-        last_role = messages[-1]['role']
-        second_last_role = messages[-2]['role']
-        if len(messages) > 2:
-            if last_role == second_last_role:
-                messages = messages[:-1]
+        #last_role = messages[-1]['role']
+        #second_last_role = messages[-2]['role']
+        #if len(messages) > 2:
+        #    if last_role == second_last_role:
+        #        messages = messages[:-1]
 
-        messages = [concat_text_lists(m) for m in messages]
+        #messages = [concat_text_lists(m) for m in messages]
 
         stream = await client.chat.completions.create(
             model=model_name,
